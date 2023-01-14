e2f4e2b9425aaa5c2023fa01a6d9b6ad6479d727
unknown <holydexboi@yahoo.com>
2023-01-14 01:02:03 +0100

    first commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className='w-auto h-96 p-10 bg-gradient-to-r relative from-gradient1 to-gradient2 rounded-3xl'>
+            <div className='w-64 h-64 rounded-full bg-gradient-to-r from-balancegrad1 to-balancegrad2'>
+                
+            </div>
+            <div className='w-64 h-64 rounded-full absolute bottom-16 left-10 bg-primary'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': '#E6F2F8',
+      'gradient1': '#0995E4',
+      'gradient2': '#FFFFFF',
+      'balancegrad1': '#84FC57',
+      'balancegrad2': '#FFFFFF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
