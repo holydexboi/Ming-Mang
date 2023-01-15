6a9ae6bde7748330e8de21d1502ce19292e1a59b
unknown <holydexboi@yahoo.com>
2023-01-15 23:17:19 +0100

    recharge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {  Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Navbar from "./pages/Navbar";
 import Purchase from "./pages/Purchase";
+import Recharge from "./pages/Recharge";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Dashboard />} /> 
         <Route path="/purchase" element={<Purchase/>}/>
+        <Route path="/recharge" element={<Recharge/>}/>
       </Routes>
     </>
   );
